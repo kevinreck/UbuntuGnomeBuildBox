3988c9e176d36989b54b5793b2f9f7d214821d50
Kevin Reck <kreck@concurrency.com>
2019-08-22 15:00:45 -0500

    Updates - 3.4

--- mongoShell.js
+++ mongoShell.js
@@ -1,5 +1,6 @@
 use CV_Device;
 db.createUser({ user: "device_user" , pwd: "coleslaw", roles: ["readWrite", "userAdmin"]});
 
+
 use cvMqttBroker;
 db.createUser({ user: "mqtt" , pwd: "coleslaw", roles: ["readWrite", "userAdmin"]});
